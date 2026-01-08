c19bbc5182b5784eb16155dfa974c9b5fca3b745
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2026-01-08 16:42:49 -0300

    feat(schedule): Refactor schedule generation logic and remove JSON dependency

--- main.py
+++ main.py
@@ -1,144 +1,89 @@
 from ortools.sat.python import cp_model
-import json
-import csv
-
-model = cp_model.CpModel()
 
 
-with open('scenario.json',"r",encoding="utf-8") as file:
-    data = json.load(file)
-
-teachers_map = {t["id"]: t["name"] for t  in data["teachers"]}
-teachers_id = list(teachers_map.keys())
-
-classes_map = {c['id']: c['name'] for c in data['classes']}
-classes_id = list(classes_map.keys())
-
-slots = data["slots"]
-
-# --- ADAPTAÇÃO DOS REQUIREMENTS ---
-# Como o JSON usa NOMES, mas o solver agora quer IDs, precisamos converter.
-# Criamos um mapa reverso temporário: {'Ana Andrade': 'p1'}
-name_to_id_teacher = {v: k for k, v in teachers_map.items()}
-name_to_id_class = {v: k for k, v in classes_map.items()}
-
-requirements = []
-for req in data["requirements"]:
-    # traduz o nome para ID antes de guardar
-    t_id = name_to_id_teacher[req['teacher_name']]
-    c_id = name_to_id_class[req['class_name']]
-    count = req['count']
-    requirements.append((t_id, c_id, count))
-
-combinations = {}
-
-for teacher in teachers_id:
-    for i_class in classes_id:
-        for slot in slots:
-            var_name = f"t_{teacher}_c_{i_class}_s_{slot}"
-            var = model.NewBoolVar(var_name)
-            
-            combinations[(teacher, i_class, slot)] = var
-
-
-for teacher in teachers_id:
-    for slot in slots:
-        var_for_sum = []
-
-        for i_class in classes_id:
-            var = combinations[(teacher,i_class,slot)]
-
-            var_for_sum.append(var)
-
-        model.Add(sum(var_for_sum) <= 1)
-
-
-
-for i_class in classes_id:
-    for slot in slots:
-        var_for_sum = []
-
-        for teacher in teachers_id:
-            var = combinations[(teacher,i_class,slot)]
-
-            var_for_sum.append(var)
-
-        model.Add(sum(var_for_sum) <= 1)
-            
-            
-for requirement in requirements:
-    var_for_sum = []    
-    for slot in slots:
-        var = combinations[(requirement[0],requirement[1],slot)]
-
-        var_for_sum.append(var)
-
-    model.Add(sum(var_for_sum) == requirement[2])
-
-
-solver = cp_model.CpSolver()
-
-status = solver.Solve(model)
-
-
-
-if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    print("\n" + "="*60)
-    print(f"{'HORÁRIO':<15} |", end="") # Cabeçalho da primeira coluna
+def generate_schedule(teachers, classrooms, slots, requirements):
+    """
+    Docstring for generate_schedule
     
-    # Cabeçalho das colunas (Nomes das Turmas)
-    for i_class in classes_id:
-        c_name = classes_map[i_class]
-        print(f" {c_name:<15} |", end="")
-    print("\n" + "-"*85)
+    :param teachers: dict list with {id,name}
+    :param classrooms: dict list with {id,name}
+    :param slots: class schedules
+    :param requirements: dict list of the number of classes each teacher teaches to a group of students.
 
-    # CORPO DA TABELA
-    # Agora a linha é o SLOT (Horário)
-    for slot in slots:
-        row_text = f"{slot:<15} |"
-        
-        # E as colunas são as TURMAS
-        for i_class in classes_id:
-            teacher_found = " - "
-            
-            # Buscamos quem dá aula nessa turma, nesse horário
+    Receives lists of data and returns the resolved time.
+    """
+
+    model = cp_model.CpModel()
+
+    teachers_map = {t["id"]: t["name"] for t in teachers}
+    teachers_id = list(teachers_map.keys())
+
+    classrooms_map = {c["id"] : c["name"] for c in classrooms}
+    classrooms_id = list(classrooms_map.keys())
+
+    schedule_variables = {}
+
+    for teacher in teachers_id:
+        for classroom in classrooms_id:
+            for slot in slots:
+                var_name = f"t_{teacher}_c_{classroom}_s_{slot}"
+                var = model.NewBoolVar(var_name)
+
+                schedule_variables[(teacher,classroom,slot)] = var
+
+    for teacher in teachers_id:
+        for slot in slots:
+            var_for_sum = []
+
+            for classroom in classrooms_id:
+                var = schedule_variables[(teacher,classroom,slot)]
+
+                var_for_sum.append(var)
+
+            model.Add(sum(var_for_sum) <=1)
+
+    
+    for classroom in classrooms_id:
+        for slot in slots:
+            var_for_sum = []
+
             for teacher in teachers_id:
-                if solver.Value(combinations[(teacher, i_class, slot)]) == 1:
-                    teacher_found = teachers_map[teacher]
-                    break
+                var = schedule_variables[(teacher,classroom,slot)]
+
+                var_for_sum.append(var)
             
-            # Truque para encurtar nomes muito longos (pega só os primeiros 15 chars)
-            row_text += f" {teacher_found[:15]:<15} |"
-        
-        print(row_text)
-        
-        # Adiciona uma linha separadora entre os dias para facilitar a leitura
-        if "10" in slot: # Se for o último horário do dia
-            print("-" * 85)
+            model.Add(sum(var_for_sum) <= 1)
 
-
-    with open('horario_gerado.csv',"w",newline="",encoding="utf-8") as csv_file:
-        writer = csv.writer(csv_file,delimiter=";")
-
-        header_row = ['Horário'] + [classes_map[c_id] for c_id in classes_id]
-
-        writer.writerow(header_row)
+    for requirement in requirements:
+        var_for_sum = []
 
         for slot in slots:
-            row_data = [slot]
+            var = schedule_variables[(requirement[0],requirement[1],slot)]
 
-            for i_class in classes_id:
-                teacher_name = ""
+            var_for_sum.append(var)
 
-                for teacher in teachers_id:
-                    if solver.Value(combinations[(teacher,i_class,slot)]) == 1:
-                        teacher_name = teachers_map[teacher]
-                        break
-                row_data.append(teacher_name)
+        model.Add(sum(var_for_sum) == requirement[2])
 
-            writer.writerow(row_data)
     
-    print("\nArquivo 'horario_gerado.csv' criado com sucesso!")
-else:
-    print("Não foi possível encontrar uma solução.")
+
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        
+        final_schedule = []
+
+        for slot in slots:
+            for classroom in classrooms_id:
+                for teacher in teachers_id:
+                    if solver.Value(schedule_variables[(teacher,classroom,slot)]) == 1:
+                        final_schedule.append({
+                            "time" : slot,
+                            "classroom" : classrooms_map[classroom],
+                            "teacher" : teachers_map[teacher]
+                        })
+
+        return final_schedule
+    else:
+        return None
 
